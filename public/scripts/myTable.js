1deff89a19c97a326654bbf0e8d6251f165115da
ygliu2016 <yliu@scclin011.flatironinstitute.org>
2021-02-16 21:21:32 -0500

    interface

--- public/scripts/myTable.js
+++ public/scripts/myTable.js
@@ -54,7 +54,7 @@ function getDisplayFloat (n) {
 function createMultiTitle (data_dict, parent_id, job_id) {
    var pas = d3.select(parent_id).selectAll('p')
                .data(Object.keys(data_dict))
-               .enter().append('div').attr('class', 'section__div')
+               .enter().append('div')
    pas.append('p')
       .attr('class', 'thick table_title')
       .html(function (d) {
@@ -90,6 +90,7 @@ function createMultiTable (data_dict, parent_id, table_title_list, job_id) {
          .attr('class', function(d,i) {return 'noborder ' + table_title_list[i]})
          .text(function (d) {return d}) //TODO: change to createMultiTable2 style
 
+   console.log("--done createMutliTable")
    collapseTitle ()
 }
 
@@ -159,6 +160,7 @@ function createMultiTable2 (data_dict, parent_id, table_title_list, node, type_l
              return d
          })
 
+   
    // collapse behavior
    collapseTitle ()
 }
