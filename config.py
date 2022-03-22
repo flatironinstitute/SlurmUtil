0577d09e17e430d54a7e4b04ef5d3a27224db28a
ygliu2016 <yanbin_liu@yahoo.com>
2022-03-22 16:29:52 -0400

    change config

--- config.py
+++ config.py
@@ -15,29 +15,27 @@ SUMMARY_TABLE_COL = ['node', 'status', 'delay', 'node_mem_M', 'job', 'user', 'al
 
 
 def fillDefSettings (conf):
+    defSettings = getUserSettings("default")
     if "settings" not in conf:
        conf["settings"] = {}
     settings = conf["settings"]
        
     if "low_util_job"  not in settings:  #percentage
-       settings["low_util_job"]      = {"cpu":1, "gpu":10, "mem":30, "run_time_hour":24, "alloc_cpus":30, "email":False}
+       settings["low_util_job"]      = defSettings["low_util_job"]
     if "low_util_node" not in settings:
-       settings["low_util_node"]     = {"cpu":1, "gpu":10, "mem":30, "alloc_time_min":60}
+       settings["low_util_node"]     = defSettings["low_util_node"]
     if "summary_column" not in settings:
-       settings["summary_column"]    = dict(zip(SUMMARY_TABLE_COL, [True]*len(SUMMARY_TABLE_COL)))
-       settings["summary_column"]['delay']        = False  #delay not showing
-       settings["summary_column"]['avg_gpu_util'] = False  #disable avg_gpu_util
+       settings["summary_column"]    = defSettings["summary_column"]
     if "summary_low_util" not in settings:
-       settings["summary_low_util"]  = {"cpu_util":1, "avg_cpu_util":1, "rss":1, "gpu_util":10, "avg_gpu_util":10, "type":"inform"}
+       settings["summary_low_util"]  = defSettings["summary_low_util"]
     if "summary_high_util" not in settings:
-       settings["summary_high_util"] = {"cpu_util":90,"avg_cpu_util":90, "rss":90, "gpu_util":90, "avg_gpu_util":90, "type":"alarm"}
+       settings["summary_high_util"] = defSettings["summary_high_util"]
     if "heatmap_avg" not in settings:
-       settings["heatmap_avg"]       = {"cpu":0,"gpu":5}
+       settings["heatmap_avg"]       = defSettings["heatmap_avg"]
     if "heatmap_weight" not in settings:
-       settings["heatmap_weight"]    = {"cpu":50,"mem":50}
+       settings["heatmap_weight"]    = defSettings["heatmap_weight"]
     if "part_avail" not in settings:
-       settings["part_avail"]        = {"node":10,"cpu":10,"gpu":10}
-    return settings
+       settings["part_avail"]        = defSettings["part_avail"]
 
 def getUserSettings (user):
     filename = os.path.join(APP_DIR, 'config/{}_settings.json'.format(user))
@@ -45,7 +43,8 @@ def getUserSettings (user):
        with open(filename) as setting_file:
             return json.load(setting_file)
     else:
-       return APP_CONFIG["settings"]
+       return APP_CONFIG["display_default"]
+
 def readConfigFile (configFile):
    configFile = os.path.join(APP_DIR, configFile)
    if os.path.isfile(configFile):
@@ -53,7 +52,7 @@ def readConfigFile (configFile):
           cfg= json.load(config_file)
       for key, val in cfg.items():
           APP_CONFIG[key] = val    #overwrite the old value if there is any
-      fillDefSettings (APP_CONFIG)
+      #fillDefSettings (APP_CONFIG)
       if APP_NAME in APP_CONFIG['log']:
          logLevel = eval(APP_CONFIG['log'][APP_NAME])
       else:
