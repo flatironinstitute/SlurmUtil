cf260f4dc8af5686bdc1d0bb92d1b89c1604442d
ygliu2016 <yanbin_liu@yahoo.com>
2023-04-13 16:18:33 -0400

    rewrite to simply login and use multiprocessing.Manager

--- mqttMonStream.py
+++ mqttMonStream.py
@@ -2,6 +2,7 @@ import argparse
 import _pickle as cPickle
 import urllib.request as urllib2
 import json, logging, os, pwd, sys, threading, time, zlib
+import multiprocessing
 import paho.mqtt.client as mqtt
 import pyslurm
 import pdb
@@ -13,27 +14,23 @@ from IndexedDataFile import IndexedHostData
 from mqttMon2Influx import Node2PidsCache
 
 logger   = config.logger        #use app name, report to localhost:8126/data/log
+#MyTool.getFileLogger('mqttMonitor', logging.DEBUG)  # use module name
 
-#MyTool.getFileLogger('mqttMonStream', logging.DEBUG)  # use module name
-
-# Maps a host name to a tuple (time, prePid2info), where time is the time
-# stamp of the last update, and prePid2info maps a pid to a dictionary of
-# info about the process identified by the pid.
-
-class MQTTReader(threading.Thread):
-    def __init__(self, mqtt_server):
-        threading.Thread.__init__(self)
+class MQTTReader(multiprocessing.Process):
+    def __init__(self, mqtt_server, mqtt_msgs):
+        super().__init__()
         # connect mqtt
+        self.name                   = "MQTTReader"
         self.mqtt_client            = mqtt.Client()
         self.mqtt_client.on_connect = self.on_connect
         self.mqtt_client.on_message = self.on_message
         self.mqtt_client.connect(mqtt_server)
         # save incoming messages
-        self.msgs      = DDict(list)  # host: msgs
-        self.lock      = threading.Lock()   #guard the message list hostperf_msgs, ...
+        self.msgs                   = mqtt_msgs  # host: msgs
 
     def run(self):
         # Asynchronously receive messages
+        print("Start MQTTReader")
         logger.info("MQTTReader start the loop_forever ...")
         self.mqtt_client.loop_forever()   #loop_forever()  method blocks the program, handles automatic reconnects.
 
@@ -41,54 +38,48 @@ class MQTTReader(threading.Thread):
         logger.info("MQTTReader on_connect with code {}.".format(rc))
         self.mqtt_client.subscribe("cluster/hostprocesses/#")
 
-    #running in the MainThread
     def on_message(self, client, userdata, msg):
         data     = json.loads(msg.payload)
         hostname = data['hdr']['hostname']
-        #logger.debug("on_message {}:{}:{}.".format(threading.get_ident(), MyTool.getTsString(data['hdr']['msg_ts']), hostname))
-        #if hostname.startswith('worker'):
-        with self.lock:
-             self.msgs[hostname].append(data)
+        msg_ts   = data['hdr']['msg_ts']
+        if hostname in self.msgs:
+            last_msg = self.msgs[hostname][-1]
+            if last_msg['hdr']['msg_ts'] < msg_ts:
+               self.msgs[hostname] = [last_msg, data]       # keep 2 values per time
+        else:
+            self.msgs[hostname] = [data]
 
-    def retrieveMsgs(self):
-        with self.lock:
-            logger.debug("retrieveMsgs {}".format(len(self.msgs)))
-            sav       = self.msgs
-            self.msgs = DDict(list)
-        return sav
-        
-class FileWebUpdater(threading.Thread):
-    INTERVAL   = 60
-
-    def __init__(self, source, tgt_dir, tgt_url, write_file_flag, extra_pyslurm, cfg):
-        threading.Thread.__init__(self)
-        self.source          = source
-        self.hostData_dir    = IndexedHostData(tgt_dir)           # write to files in the data_dir
-        self.urls            = tgt_url                            # send update to urls
-        self.savNode2TsProcs = DDict(lambda: (-1.0, {}, []))   #host - ts - pid - proc
-        self.write_file_flag = write_file_flag
-        self.extra_pyslurm   = extra_pyslurm
-        self.config          = cfg
-        logger.info("Start FileWebUpdater with tgt_dir={}, urls={}, extra_pyslurm={}, write_file_flag={}".format(tgt_dir, self.urls, extra_pyslurm, write_file_flag))
+class MQTTUpdater(multiprocessing.Process):
+    #write mqttMsg every interval seconds
+    def __init__(self, mqttMsg, interval, config):
+        super().__init__()
+        self.name     = "MQTTUpdater"
+        self.data     = mqttMsg
+        self.interval = interval
+        self.config   = config
 
     def run(self):
-        #pdb.set_trace()
+        print("Start {}".format(self.name))
         while True:
-            msgs       = self.source.retrieveMsgs()
-            if msgs:
-               curr_ts     = int(time.time())
-               pyslurmData = self.getPyslurmData ()
-               self.dealData (curr_ts, msgs, pyslurmData)
-            time.sleep(self.INTERVAL)
+            print("\t{} Send update".format(self.name))
+            self.sendUpdate ()
+            time.sleep(self.interval)
 
+    # get pyslurm data
     def getPyslurmData (self):
         pyslurm.slurm_init()
-        if self.extra_pyslurm:
-           pyslurmData    = {'jobs':pyslurm.job().get(), 'nodes':pyslurm.node().get(), 'partition':pyslurm.partition().get(), 'qos':pyslurm.qos().get(), 'reservation':pyslurm.reservation().get(), 'extra_pyslurm':True}
-        else:
-           pyslurmData    = {'jobs':pyslurm.job().get(), 'nodes':pyslurm.node().get(), 'extra_pyslurm':False}
+        pyslurmData    = {'jobs':pyslurm.job().get(), 'nodes':pyslurm.node().get(), 'partition':pyslurm.partition().get(), 'qos':pyslurm.qos().get(), 'reservation':pyslurm.reservation().get()}
         return pyslurmData
 
+    def sendUpdate (self):
+        print("\t{} Send update".format(self.name))
+
+class WebUpdater(MQTTUpdater):
+    def __init__(self, mqttMsg, interval, config):
+        super().__init__(mqttMsg, interval, config)
+        self.name   = "WebUpdater"
+
+    # get user's alloc cpu count on each node {'node': {'user': cpu_count}}
     def getUserAllocCPUOnNode(self, slurmJobs):
         runningJobs     = [job for job in slurmJobs.values() if job['job_state']=='RUNNING']
         node2uid2cpuCnt = DDict(lambda: DDict(int))     #node-uid-allocatedCPUCount
@@ -97,10 +88,10 @@ class FileWebUpdater(threading.Thread):
                node2uid2cpuCnt[node][job['user_id']] += c
         return node2uid2cpuCnt
 
-    # return the proces on hostname
+   # return user's proces on node hostname
     def getProcsByUser (self, hostname, msg_ts, msg_procs, pre_ts, pre_procs, uid2cpuCnt):
 
-        procsByUser = []                 # [[user, uid, alloc_cores, proc_cnt, totCPURate, totRss, totVMS, procs, totIOBps, totCPUTime]...] 
+        procsByUser = []                 # [[user, uid, alloc_cores, proc_cnt, totCPURate, totRss, totVMS, procs, totIOBps, totCPUTime]...]
         uid2procs   = DDict(list)        # uid - [[pid, CPURate, create_time, user_time, system_time, rss, vms, cmdline, IOBps]...]
         for pid, proc in msg_procs.items():
             if pid in pre_procs:  # continue proc
@@ -133,101 +124,119 @@ class FileWebUpdater(threading.Thread):
             totVMS     = sum([proc[6]         for proc in procs])
             totIOBps   = sum([proc[8]         for proc in procs])
             procsByUser.append ([MyTool.getUser(uid), uid, uid2cpuCnt.get(uid,0), len(procs), totCPURate, totRSS, totVMS, procs, totIOBps, totCPUTime])
-            
+
         return procsByUser
 
-    def getPreData (self, hostname, host_msgs):
-        pre_ts, pre_procs, pre_nodeUserProcs = self.savNode2TsProcs[hostname]
-        if len(host_msgs) > 1: 
-           older_msg = host_msgs[-2]
-           older_ts  = older_msg['hdr']['msg_ts']
-           if (older_ts > pre_ts) and ( older_ts < host_msgs[-1]['hdr']['msg_ts'] ):  #saved value is older
-              pre_ts            = older_ts
-              pre_procs         = dict([(proc['pid'], proc) for proc in older_msg['processes']]) 
-        return pre_ts, pre_procs
-
-    def savPreData (self, hostname, msg_ts, msg_procs, nodeUserProcs):
-        self.savNode2TsProcs[hostname] = (msg_ts, msg_procs, nodeUserProcs)
-
-    def dealData(self, ts, msgs, pyslurmData):
-        print("dealData")
-        slurmNodes      = pyslurmData['nodes']
+    def getMsgProcs (self, msg):
+        msg_ts      = msg['hdr']['msg_ts']
+        msg_procs   = dict([(proc['pid'], proc) for proc in msg['processes']]) 
+        return msg_ts, msg_procs
+ 
+    def collectData(self):
+        ts              = int(time.time())
+        pyslurmData     = self.getPyslurmData ()
         node2uid2cpuCnt = self.getUserAllocCPUOnNode(pyslurmData['jobs'])
         nodeUserProcs   = {} # reported data {node: [state, delta, ts, [user, procs]]} 
         #update the information using msg
-        for hostname in list(msgs.keys()):
-            slurmNode   = slurmNodes.get(hostname, None)
+        for hostname in list(self.data.keys()):
+            slurmNode   = pyslurmData['nodes'].get(hostname, None)
             if self.config.get('slurmOnly', True) and not slurmNode:
-               print("skip no-slurm node {}".format(hostname))
+               logger.info("skip no-slurm node {}".format(hostname))
                continue
             slurm_state = slurmNode.get('state', '?STATE?') if slurmNode else 'NO_SLURM'
-            host_msgs   = msgs.pop(hostname)
-            msg         = host_msgs[-1]               # get the latest message
-            msg_ts      = msg['hdr']['msg_ts']
-            msg_procs   = dict([(proc['pid'], proc) for proc in msg['processes']]) 
-            pre_ts, pre_procs = self.getPreData(hostname, host_msgs)
-            delta       = 0.0 if -1.0 == pre_ts else msg_ts - pre_ts
-            procsByUser = self.getProcsByUser (hostname,msg_ts,msg_procs,pre_ts,pre_procs,node2uid2cpuCnt.get(hostname,{}))
-            nodeUserProcs[hostname] = [slurm_state, delta, msg_ts] + procsByUser
-            self.savPreData (hostname, msg_ts, msg_procs, nodeUserProcs[hostname])
-
-            #save information to files
-            #logger.debug("writeData {}:{}".format(ts, hostname)) 
-            if self.write_file_flag:
-               self.hostData_dir.writeData(hostname, ts, nodeUserProcs[hostname])
+            msg_ts, msg_procs = self.getMsgProcs     (self.data[hostname][-1])
+            if len(self.data[hostname]) > 1:
+                pre_ts, pre_procs = self.getMsgProcs (self.data[hostname][0])
             else:
-               logger.debug("simulate write to file")
+                pre_ts, pre_procs = msg_ts, {}
+            procsByUser = self.getProcsByUser (hostname,msg_ts,msg_procs,pre_ts,pre_procs,node2uid2cpuCnt.get(hostname,{}))
+            nodeUserProcs[hostname] = [slurm_state, msg_ts - pre_ts, msg_ts] + procsByUser
+        return ts, nodeUserProcs, pyslurmData
 
-        self.sendUpdate    (ts, nodeUserProcs, pyslurmData)
-        self.discardMessage(msgs)
-
-    def sendUpdate (self, ts, hn2data, pyslurmData):
-        if not self.config.get("sendUpdate", True):
-           print ("send data: \n{}\n{}".format(ts, hn2data.keys()))
-           return
-
-        for url in self.urls:
-           logger.info("compress sent data");
-           zps = zlib.compress(cPickle.dumps((ts, hn2data, pyslurmData), -1))
+    def sendData (self, ts, nodeUserProc, pyslurmData):
+        for url in self.config.get("webURL", []):
+           zps = zlib.compress(cPickle.dumps((ts, nodeUserProc, pyslurmData), -1))
            try:
-               logger.debug("sendUpdate to {}".format(url))
+               print("sendUpdate to {}".format(url))
                resp = urllib2.urlopen(urllib2.Request(url, zps, {'Content-Type': 'application/octet-stream'}))
-               logger.debug("{}:{}: sendUpdate to {} with return code {}".format(threading.current_thread().ident, MyTool.getTsString(ts), url, resp))
+               logger.debug("{}: sendUpdate to {} with return code {}".format(MyTool.getTsString(ts), url, resp))
+               print("{}: sendUpdate to {} with return code {}".format(MyTool.getTsString(ts), url, resp))
            except Exception as e:
                body = e
                logger.error( 'Failed to update slurm data {}: {}\n{}'.format(url, e, body))
 
-    def discardMessage(self, msgs):
-        hdiscard, mmdiscard = 0, 0
-        #self.msgs is a dict
-        while (len(msgs) > 0):
-            h, value = msgs.popitem()
-            hdiscard  += 1
-            mmdiscard += len(value)
-        if hdiscard: 
-           logger.info('Discarding %d messages from %d hosts (e.g., %s)'%(mmdiscard, hdiscard, h))
-           print('Discarding %d messages from %d hosts (e.g., %s)'%(mmdiscard, hdiscard, h))
+    def sendUpdate (self):
+        ts, nodeUserProc, pyslurmData = self.collectData ()
+        self.sendData (ts, nodeUserProc, pyslurmData)
 
-def main(uiServer, mqtt_dict, extra_pyslurm, cfg):
-    source = MQTTReader (mqtt_dict['host'])
-    source.start()
-    time.sleep(5)
-    app    = FileWebUpdater(source, mqtt_dict['file_dir'], uiServer, mqtt_dict['writeFile'], extra_pyslurm, cfg)
-    app.start()
+class FileWebUpdater(WebUpdater):
+    def __init__(self, mqttMsg, interval, config):
+        super().__init__(mqttMsg, interval, config)
+        self.name = "FileWebUpdater"
+        self.hostData_dir = IndexedHostData(config["file_dir"])                # directory for backup files
+
+    def sendData (self, ts, nodeUserProc, pyslurmData):
+        super().sendData (ts, nodeUserProc, pyslurmData)
+        for hostname, nodeProc in nodeUserProc.items():
+            msg_ts = nodeProc[2]                # [slurm_state, delta, msg_ts] + procsByUser
+            self.hostData_dir.writeData(hostname, msg_ts, nodeProc)
+
+class InfluxUpdater(MQTTUpdater):
+    def __init__(self, mqttMsg, interval, config):
+        super().__init__(mqttMsg, interval, config)
+        self.name = "InfluxUpdater"
+
+class UpdateProcessManager:
+    INTERVAL = 60
+    def __init__(self, config):
+        self.config = config
+
+    def start(self):
+        print("config={}".format(self.config))
+        writeWebProc, writeFileWebProc, writeInfluxProc = None, None, None
+        with multiprocessing.Manager() as manager:
+            # start mqtt client
+            mqttMsg        = manager.dict()
+            mqttReaderProc = MQTTReader (self.config["server"], mqttMsg)
+            mqttReaderProc.start()
+            # start updater processes based on config file
+            if self.config.get("writeWeb", False):
+                if not self.config.get("writeFile", False):
+                   writeWebProc     = WebUpdater     (mqttMsg, self.INTERVAL,  self.config)
+                   writeWebProc.start()
+                else:
+                   writeFileWebProc = FileWebUpdater (mqttMsg, self.INTERVAL*2,self.config)
+                   writeFileWebProc.start()
+            if self.config.get("writeInflux", False):
+                writeInfluxProc = InfluxUpdater (mqttMsg, self.INTERVAL*3,self.config)
+                writeInfluxProc.start()
+
+            # restart the processes if they die
+            while True:
+                if not mqttReaderProc.is_alive():
+                   mqttReaderProc = MQTTReader (mqttMsg)
+                   mqttReaderProc.start()
+                if writeWebProc and not writeWebProc.is_alive():
+                   writeWebProc = WebUpdater       (mqttMsg, self.INTERVAL,  self.config)
+                   writeWebProc.start()
+                if writeFileWebProc and not writeFileWebProc.is_alive():
+                   writeFileWebProc = FileWebUpdater  (mqttMsg, self.INTERVAL*2,self.config)
+                   writeFileWebProc.start()
+                if writeInfluxProc and not writeInfluxProc.is_alive():
+                   writeInfluxProc = InfluxUpdater (mqttMsg, self.INTERVAL*3,self.config)
+                   writeInfluxProc.start()
 
 if __name__=="__main__":
    #Usage: python mqttMonStream.py 
-   parser = argparse.ArgumentParser (description='Start a deamon to save mqtt and pyslurm information in file and report to user interface.')
+   parser = argparse.ArgumentParser (description='Start a deamon to listen on mqtt, retrieve pyslurm data and send data to web, file and/or influxdb.')
    parser.add_argument('-c', '--configFile',  help='The name of the config file.')
    args       = parser.parse_args()
    configFile = args.configFile
    if configFile:
       config.readConfigFile(configFile)
    cfg   = config.APP_CONFIG
-   f_dir = cfg["mqtt"]["file_dir"]
-   if f_dir and not os.path.isdir(f_dir):
-      os.mkdir(f_dir)
-   main(cfg['ui']['urls'], cfg['mqtt'], cfg['ui']['extra_pyslurm_data'], cfg["mqtt"])
+   m = UpdateProcessManager(cfg["mqtt"])
+   m.start()
 
  
 
